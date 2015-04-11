4147ed8f1b2f52648e1d8a96dcb502551f5f65a3
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-04-11 08:30:42 -0500

    add submodule and create projects

---
+++ CoreCerraduraServer/CoreCerraduraServer/CoreCerraduraServer.h
@@ -0,0 +1,19 @@
+//
+//  CoreCerraduraServer.h
+//  CoreCerraduraServer
+//
+//  Created by Alsey Coleman Miller on 4/11/15.
+//  Copyright (c) 2015 ColemanCDA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CoreCerraduraServer.
+FOUNDATION_EXPORT double CoreCerraduraServerVersionNumber;
+
+//! Project version string for CoreCerraduraServer.
+FOUNDATION_EXPORT const unsigned char CoreCerraduraServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreCerraduraServer/PublicHeader.h>
+
+
