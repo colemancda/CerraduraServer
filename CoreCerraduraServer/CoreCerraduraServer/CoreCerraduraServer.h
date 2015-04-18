4c519c5afc7884cabb6b73ea5d64074738336f66
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-04-18 13:06:23 -0500

    update submodule

--- CoreCerraduraServer/CoreCerraduraServer/CoreCerraduraServer.h
+++ CoreCerraduraServer/CoreCerraduraServer/CoreCerraduraServer.h
@@ -14,6 +14,4 @@ FOUNDATION_EXPORT double CoreCerraduraServerVersionNumber;
 //! Project version string for CoreCerraduraServer.
 FOUNDATION_EXPORT const unsigned char CoreCerraduraServerVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <CoreCerraduraServer/PublicHeader.h>
-
 
