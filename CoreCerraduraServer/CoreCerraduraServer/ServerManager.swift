819d8a252b7c24ee718740e23c1e581e56beaa41
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-04-20 20:26:08 -0500

    added ServerManager

---
+++ CoreCerraduraServer/CoreCerraduraServer/ServerManager.swift
@@ -0,0 +1,18 @@
+//
+//  ServerManager.swift
+//  CoreCerraduraServer
+//
+//  Created by Alsey Coleman Miller on 4/20/15.
+//  Copyright (c) 2015 ColemanCDA. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import NetworkObjects
+import CoreCerradura
+
+/* Manages the incoming connections to the server. */
+public class ServerManager, ServerDelegate {
+    
+    
+}
