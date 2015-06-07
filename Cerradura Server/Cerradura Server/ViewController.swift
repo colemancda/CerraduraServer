47bda2a0f274e9e3536fe49bf104b58d2c9fe700
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-06-07 13:28:13 -0500

    created Admin GUI app

---
+++ Cerradura Server/Cerradura Server/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Cerradura Server
+//
+//  Created by Alsey Coleman Miller on 6/7/15.
+//  Copyright (c) 2015 ColemanCDA. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
