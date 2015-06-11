2067b18c7f9188271f9282a032e9a06c3b701ffc
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-06-10 22:09:26 -0500

    added Log.swift

---
+++ CoreCerraduraServer/CoreCerraduraServer/Log.swift
@@ -0,0 +1,14 @@
+//
+//  Log.swift
+//  CoreCerraduraServer
+//
+//  Created by Alsey Coleman Miller on 6/10/15.
+//  Copyright (c) 2015 ColemanCDA. All rights reserved.
+//
+
+import Foundation
+
+@objc public class ServerLog {
+    
+    
+}
